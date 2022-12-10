febc20bf82985b92b779589a39a576e192774e9b
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-10 15:35:02 +0200

    Add hotels router

---
+++ api/routes/hotels.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js';
+
+const router = express.Router();
+
+router.post('/', createHotel);
+
+router.put('/:id', updateHotel);
+
+router.delete('/:id', deleteHotel);
+
+router.get('/:id', getHotel);
+
+router.get('/', getHotels);
+
+export default router;
