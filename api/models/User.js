07e7c625ccbe63f3e6dc9f82f64bdeab4f62293b
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-11 08:53:43 +0200

    Updater user model

--- api/models/User.js
+++ api/models/User.js
@@ -1,28 +1,39 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const { Schema } = mongoose;
-
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
+    },
+    country: {
+      type: String,
+    },
+    img: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    phone: {
+      type: String,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     isAdmin: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
-},
-    { timestamps: true }
+  },
+  { timestamps: true }
 );
 
-export default mongoose.model('User', UserSchema);
+export default mongoose.model("User", UserSchema);
