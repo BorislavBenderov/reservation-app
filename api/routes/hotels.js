dfe0b57d6d90dbc62cf9031b8d914f54a19e468f
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-11 08:58:02 +0200

    Update routes in hotels router

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,16 +1,19 @@
 import express from 'express';
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js';
+import { createHotel, deleteHotel, getHotel, getHotels, updateHotel, countByCity, countByType } from '../controllers/hotel.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
-router.post('/', createHotel);
+router.post('/', verifyAdmin, createHotel);
 
-router.put('/:id', updateHotel);
+router.put('/:id', verifyAdmin, updateHotel);
 
-router.delete('/:id', deleteHotel);
+router.delete('/find/:id', verifyAdmin, deleteHotel);
 
 router.get('/:id', getHotel);
 
 router.get('/', getHotels);
+router.get('/countByCity', countByCity);
+router.get('/countByType', countByType);
 
 export default router;
