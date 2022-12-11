8aaa508f2def944b61d758a2ff340105cd0c594a
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-11 08:54:11 +0200

    Add count by city and type in hotel controller

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -46,3 +46,34 @@ export const getHotels = async (req, res, next) => {
         next(error);
     }
 };
+
+export const countByCity = async (req, res, next) => {
+    const cities = req.query.cities.split(',');
+    try {
+        const list = await Promise.all(cities.map(city => {
+            return Hotel.countDocuments({ city: city })
+        }));
+        res.status(200).json(list);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const countByType = async (req, res, next) => {
+    try {
+        const hotelCount = await Hotel.countDocuments({ type: 'hotel' });
+        const apartmentCount = await Hotel.countDocuments({ type: 'apartment' });
+        const resortCount = await Hotel.countDocuments({ type: 'resort' });
+        const villaCount = await Hotel.countDocuments({ type: 'villa' });
+        const cabinCount = await Hotel.countDocuments({ type: 'cabin' });
+        res.status(200).json([
+            { type: 'hotel', count: hotelCount },
+            { type: 'apartment', count: apartmentCount },
+            { type: 'resort', count: resortCount },
+            { type: 'villa', count: villaCount },
+            { type: 'cabin', count: cabinCount }
+        ]);
+} catch (error) {
+    next(error);
+}
+};
